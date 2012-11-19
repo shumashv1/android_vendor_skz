9eccb8c6023ce99151a9858ef5541794cbd834d1
shumashv1 <shumash@comcast.net>
2012-11-18 19:03:15 -0700

    Update vendorsetup.sh
    Remove merge errors

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
 add_lunch_combo pa_galaxys2-userdebug
 add_lunch_combo pa_i9300-userdebug
 add_lunch_combo pa_maguro-userdebug
 add_lunch_combo pa_toro-userdebug
 add_lunch_combo pa_toroplus-userdebug
 add_lunch_combo pa_tenderloin-userdebug
-=======
 add_lunch_combo pa_crespo-userdebug
 add_lunch_combo pa_grouper-userdebug
 add_lunch_combo pa_maguro-userdebug
 add_lunch_combo pa_toro-userdebug
 add_lunch_combo pa_toroplus-userdebug
->>>>>>> 4ec61950cf7ab2171b19c593aac35544b318b1a9
