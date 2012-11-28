6cad649e86d85331b5d02098df2453c3a69f4543
shumash <shumash@Ubuntu_x64.(none)>
2012-11-28 14:59:10 -0700

    Add new 4.11 fingerpring and preference overlay

--- products/pa_tenderloin.mk
+++ products/pa_tenderloin.mk
@@ -10,10 +10,13 @@ $(call inherit-product, device/hp/tenderloin/device_tenderloin.mk)
 PRODUCT_RELEASE_NAME := Touchpad
 TARGET_BOOTANIMATION_NAME := horizontal-1024x768
 
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_large-mdpi
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=touchpad BUILD_FINGERPRINT=hp/hp_tenderloin/tenderloin:4.0.4/IMM76I/330937:user/release-keys PRIVATE_BUILD_DESC="tenderloin-user 4.0.4 IMM76I 330937 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=touchpad BUILD_FINGERPRINT=hp/hp_tenderloin/tenderloin:4.1.1/JR003C/228551:user/release-keys:user/release-keys PRIVATE_BUILD_DESC="tenderloin-user 4.1.1 JR003C 228551 release-keys"
 
 PRODUCT_NAME := pa_tenderloin
 PRODUCT_DEVICE := tenderloin
