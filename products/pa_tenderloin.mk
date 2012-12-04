84a715ef2f5a0e4e8c6a40167e408bb5d065d85a
shumash <shumash@Ubuntu_x64.(none)>
2012-12-04 09:38:27 -0700

    Fix PRODUCT_COPY_FILE destination path

--- products/pa_tenderloin.mk
+++ products/pa_tenderloin.mk
@@ -13,7 +13,7 @@ TARGET_BOOTANIMATION_NAME := horizontal-1024x768
 # OVERLAY_TARGET adds overlay asset source and copy files required to build
 OVERLAY_TARGET := pa_tenderloin
 PRODUCT_COPY_FILES += \
-    vendor/pa/overlay/common/packages/inputmethods/LatinIME/java/res/values/gesture-input.xml: packages/inputmethods/LatinIME/java/res/gesture-input.xml \
+    vendor/pa/overlay/common/packages/inputmethods/LatinIME/java/res/values/gesture-input.xml: packages/inputmethods/LatinIME/java/res/values/gesture-input.xml \
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
