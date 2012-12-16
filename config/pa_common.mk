f6adf7ae682a7a58fd027bd1376823c4b2f76cc2
shumash <shumash@ubuntu.(none)>
2012-12-16 05:35:27 -0800

    Revert to PA base

--- config/pa_common.mk
+++ config/pa_common.mk
@@ -10,6 +10,21 @@ PRODUCT_COPY_FILES += \
     vendor/pa/prebuilt/common/apk/SuperSU.apk:system/app/SuperSU.apk \
     vendor/pa/prebuilt/common/xbin/su:system/xbin/su
 
+# init.d support
+PRODUCT_COPY_FILES += \
+    vendor/pa/prebuilt/common/bin/sysinit:system/bin/sysinit \
+    vendor/pa/prebuilt/common/etc/init.pa.rc:root/init.pa.rc
+
+# userinit support
+PRODUCT_COPY_FILES += \
+    vendor/pa/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/pa/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
+    vendor/pa/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/pa/prebuilt/common/bin/50-backupScript.sh:system/addon.d/50-backupScript.sh
+
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \
     vendor/pa/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
@@ -18,31 +33,14 @@ PRODUCT_COPY_FILES +=  \
 # Bring in all video files
 $(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
 
-# ROM stamp
-$(shell shuf -i 0-100000 -n 1 > .stamp)
-
 # Exclude prebuilt paprefs from builds if the flag is set
 ifneq ($(PREFS_FROM_SOURCE),true)
     PRODUCT_COPY_FILES += \
-        vendor/pa/prebuilt/common/apk/ParanoidPreferences.apk:system/app/ParanoidPreferences.apk \
-        vendor/pa/prebuilt/common/apk/PerformanceControl.apk:system/app/PerformanceControl.apk \    
-        vendor/pa/prebuilt/common/apk/LatinIMEGoogle.apk:system/app/LatinIMEGoogle.apk \
-        vendor/pa/prebuilt/common/apk/LatinIMEDictionaryPack.apk:system/app/LatinIMEDictionaryPack.apk
- 
-   PRODUCT_PACKAGES += \
-        ROMControl
+        vendor/pa/prebuilt/common/apk/ParanoidPreferences.apk:system/app/ParanoidPreferences.apk
 else
     # Build paprefs from sources
     PRODUCT_PACKAGES += \
-        ParanoidPreferences \
-        ROMControl
-
-    PRODUCT_COPY_FILES += \
-        vendor/pa/prebuilt/common/apk/ParanoidPreferences.apk:system/app/ParanoidPreferences.apk \
-        vendor/pa/prebuilt/common/apk/PerformanceControl.apk:system/app/PerformanceControl.apk \    
-        vendor/pa/prebuilt/common/apk/LatinIMEGoogle.apk:system/app/LatinIMEGoogle.apk \
-        vendor/pa/prebuilt/common/apk/LatinIMEDictionaryPack.apk:system/app/LatinIMEDictionaryPack.apk
-
+        ParanoidPreferences
 endif
 
 ifneq ($(PARANOID_BOOTANIMATION_NAME),)
@@ -68,14 +66,11 @@ endif
 # device specific prebuilts
 -include vendor/pa/prebuilt/$(TARGET_PRODUCT)/prebuilt.mk
 
-#BOARD := $(subst pa_,,$(TARGET_PRODUCT))
+BOARD := $(subst pa_,,$(TARGET_PRODUCT))
 
 # ParanoidAndroid Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/pa/overlay/common
 PRODUCT_PACKAGE_OVERLAYS += vendor/pa/overlay/$(TARGET_PRODUCT)
-
-# AOKP Overlays
-PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/common_tablet
 
 # Allow device family to add overlays and use a same prop.conf
 ifneq ($(OVERLAY_TARGET),)
@@ -89,17 +84,6 @@ PRODUCT_COPY_FILES += \
     vendor/pa/prebuilt/$(PA_CONF_SOURCE).conf:system/etc/paranoid/properties.conf \
     vendor/pa/prebuilt/$(PA_CONF_SOURCE).conf:system/etc/paranoid/backup.conf
 
-### SKZ ###
-# Common Proprietary
-#PRODUCT_COPY_FILES += \
-#    vendor/pac/prebuilt/common/app/FileManager.apk:system/app/FileManager.apk
-
-BOARD := $(subst SKZ_,,$(TARGET_PRODUCT))
-
-# Add CM release version
-CM_RELEASE := true
-CM_BUILD := $(BOARD)
-
 PA_VERSION_MAJOR = 2
 PA_VERSION_MINOR = 9
 PA_VERSION_MAINTENANCE = 9
@@ -107,23 +91,22 @@ PA_VERSION_MAINTENANCE = 9
 TARGET_CUSTOM_RELEASETOOL := vendor/pa/tools/squisher
 
 VERSION := $(PA_VERSION_MAJOR).$(PA_VERSION_MINOR)$(PA_VERSION_MAINTENANCE)
-PA_VERSION := $(TARGET_PRODUCT)-$(VERSION)-$(shell date +%0d%^b%Y-%H%M%S)
-
-PAC_VERSION_MAJOR = 18
-PAC_VERSION_MINOR = 0
-PAC_VERSION_MAINTENANCE = 0
-PAC_VERSION := $(PAC_VERSION_MAJOR).$(PAC_VERSION_MINOR).$(PAC_VERSION_MAINTENANCE)
+ifeq ($(DEVELOPER_VERSION),true)
+    PA_VERSION := dev_$(BOARD)-$(VERSION)-$(shell date +%0d%^b%Y-%H%M%S)
+else
+    PA_VERSION := $(TARGET_PRODUCT)-$(VERSION)-$(shell date +%0d%^b%Y-%H%M%S)
+endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
-  ro.pac.version=$(PAC_VERSION) \
-  ro.pacrom.version=$(BOARD)_SKZ_jb-RC0-v$(PAC_VERSION)-$(shell date +%0d%^b%Y-%H%M%S) \
   ro.modversion=$(PA_VERSION) \
   ro.pa.family=$(PA_CONF_SOURCE) \
-  ro.pa.version=$(VERSION) \
-  ro.aokp.version=$(BOARD)_jb-Milestone-1
+  ro.pa.version=$(VERSION)
 
 # goo.im properties
-PRODUCT_PROPERTY_OVERRIDES += \
-  ro.goo.developerid=paranoidandroid \
-  ro.goo.rom=paranoidandroid \
-  ro.goo.version=$(shell date +%s)
+ifneq ($(DEVELOPER_VERSION),true)
+    PRODUCT_PROPERTY_OVERRIDES += \
+      ro.goo.developerid=paranoidandroid \
+      ro.goo.rom=paranoidandroid \
+      ro.goo.version=$(shell date +%s)
+endif
+
