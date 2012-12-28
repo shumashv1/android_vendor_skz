dac2d21e514c5c41550e8f19d074e38ea0b35541
shumash <shumash@Ubuntu_x64.(none)>
2012-12-28 10:00:55 -0700

    Add jb to version stamp

--- config/pa_common.mk
+++ config/pa_common.mk
@@ -94,7 +94,7 @@ VERSION := $(PA_VERSION_MAJOR).$(PA_VERSION_MINOR)$(PA_VERSION_MAINTENANCE)
 ifeq ($(DEVELOPER_VERSION),true)
     PA_VERSION := dev_$(BOARD)-$(VERSION)-$(shell date +%0d%^b%Y-%H%M%S)
 else
-    PA_VERSION := $(BOARD)-$(VERSION)-RC0-$(shell date +%0d%^b%Y-%H%M%S)
+    PA_VERSION := $(BOARD)-$(VERSION)-jb-RC0-$(shell date +%0d%^b%Y-%H%M%S)
 endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
