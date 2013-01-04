5835dbd228e788fca308ec8ba20485a9fa7cfad1
shumash <shumash@Ubuntu_x64.(none)>
2013-01-03 21:49:04 -0700

    change vendor directory name to skz

--- config/themes_common.mk
+++ config/themes_common.mk
@@ -5,4 +5,4 @@ PRODUCT_PACKAGES += \
        com.tmobile.themes 
 
 PRODUCT_COPY_FILES += \
-       vendor/pa/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
+       vendor/skz/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
