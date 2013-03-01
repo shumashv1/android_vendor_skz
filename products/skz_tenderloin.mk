b4efe2c046baf3e8477036c3c3875fb83e6ced84
shumash <shumash@Ubuntu_x64.(none)>
2013-03-01 08:25:12 -0700

    Change TP fingerprint to wingray

--- products/skz_tenderloin.mk
+++ products/skz_tenderloin.mk
@@ -37,7 +37,10 @@ OVERLAY_TARGET := skz_tenderloin
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=touchpad BUILD_FINGERPRINT=hp/hp_tenderloin/tenderloin:4.1.1/JR003C/228551:user/release-keys:user/release-keys PRIVATE_BUILD_DESC="tenderloin-user 4.1.1 JR003C 228551 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=touchpad \
+    BUILD_FINGERPRINT="motorola/tervigon/wingray:4.0.3/IML77/239789:user/release-keys" \
+    PRIVATE_BUILD_DESC="tervigon-user 4.0.3 IML77 239789 release-keys"
 
 PRODUCT_NAME := skz_tenderloin
 PRODUCT_DEVICE := tenderloin
