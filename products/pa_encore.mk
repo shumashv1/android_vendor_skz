9880b773fcea8b77c8e7eeb3a5ef900320fd8969
mateor <mateorod9@gmail.com>
2013-04-28 02:19:37 -0500

    Use the XHDPI boot animation
    The users deserve an actual PA animation, don't you think?
    
    Change-Id: Ief28f84d8df2b9755cbfa844102183ac8f09c816

--- products/pa_encore.mk
+++ products/pa_encore.mk
@@ -16,7 +16,7 @@
 ifeq (pa_encore,$(TARGET_PRODUCT))
 
 # Define PA bootanimation size
-PARANOID_BOOTANIMATION_NAME := HDPI
+PARANOID_BOOTANIMATION_NAME := XHDPI
 
 # OVERLAY_TARGET adds overlay asset source
 OVERLAY_TARGET := pa_encore
