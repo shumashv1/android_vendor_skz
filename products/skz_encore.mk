d2ca0ff2b660cfce5267a3fa7a63f7684213e17e
shumash <shumash@Ubuntu_x64.(none)>
2013-06-07 10:02:03 -0600

    AOKP PRODUCTS:  Build products in common.mk 		Add Permissions Manager to products 		Delete product builds in product mk files

--- products/skz_encore.mk
+++ products/skz_encore.mk
@@ -1,29 +1,12 @@
 # Check for target product
 ifeq (skz_encore,$(TARGET_PRODUCT))
 
-BUILD_FROM_SOURCE := true
-
 # include Schizoid common configuration
 include vendor/skz/config/skz_common.mk
 
 # Schizoid bootanimation
     PRODUCT_COPY_FILES += \
 	vendor/skz/overlay/$(TARGET_PRODUCT)/bootanimation.zip:system/media/bootanimation.zip
-
-#Check to see if we should use prebuilt
-ifneq ($(BUILD_FROM_SOURCE),true)
-    PRODUCT_COPY_FILES += \
-        vendor/skz/prebuilt/common/apk/ROMControl.apk:system/app/ROMControl.apk
-else
-#Build ROMControl from modified source
-# ROM stamp
-$(shell shuf -i 0-100000 -n 1 > .stamp)
-
-PRODUCT_PACKAGES += \
-    ROMControl
-endif
-
-
 
 # Inherit CM device configuration
 $(call inherit-product, device/bn/encore/cm.mk)
@@ -33,6 +16,7 @@ OVERLAY_TARGET := skz_encore
 
 PRODUCT_NAME := skz_encore
 PRODUCT_DEVICE := encore
+PRODUCT_BRAND := BN
 
 GET_VENDOR_PROPS := $(shell vendor/skz/tools/getvendorprops.py $(PRODUCT_NAME))
 
